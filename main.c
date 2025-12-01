6fecd48d14093911ebf49bf852e30c97dc4c334c
JonnyJKL <jonatan.sandoval.monroy@estudiante.uacm.edu.mx>
2025-11-30 22:29:38 -0600

    Agregando advertencia

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(void) {
 
     do {
         printf("\n*----------------MENU----------------*\n");
+        printf("\nAdvertencia este es un cambio critico!!!");
         printf("\n1. Capturar una lista de productos");
         printf("\n2. Buscar un producto por codigo");
         printf("\n3. Actualizar la existencia de un producto\n");
