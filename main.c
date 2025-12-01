19307bdd5d8c509dcdae7007b65119170b0b0f6b
JonnyJKL <jonatan.sandoval.monroy@estudiante.uacm.edu.mx>
2025-11-30 22:24:31 -0600

    Commit Inicial

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+    //Variable que guarda la cantidad de productos a registrar
+    int opcion;
+
+    do {
+        printf("\n*----------------MENU----------------*\n");
+        printf("\n1. Capturar una lista de productos");
+        printf("\n2. Buscar un producto por codigo");
+        printf("\n3. Actualizar la existencia de un producto\n");
+
+        printf("\nIngrese su opcion: ");
+        scanf("%d",&opcion);
+
+    }while (opcion <=0 || opcion>4);
+
+
+
+
+    printf("\nSaliste del bucle...");
+
+    return 0;
+}
